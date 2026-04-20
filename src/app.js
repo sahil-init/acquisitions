6f59e05d7bbaec714a8e6cb830ceda5801898dbc
sahil-init <sahilshinde.work@gmail.com>
2026-04-20 12:09:32 +0530

    Implement Sign-Up

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import logger from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import logger from '#config/logger.js';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
@@ -23,5 +24,19 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
